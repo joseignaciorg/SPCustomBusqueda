2a749d4ae4d60a6e8aeedb7ce0c2adcbbb8e410d
Nacho <joseignacio.rodriguez@tajamar365.com>
2016-04-26 11:14:57 +0000

    Crear un custom de busqueda en sharepoint

---
+++ CustomDeBusqueda/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="7424d881-99a4-4b28-9bf7-f7e4812318c0" description="SharePoint Add-in Feature" featureId="7424d881-99a4-4b28-9bf7-f7e4812318c0" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="CustomDeBusqueda Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="930f0253-f542-4d0a-b4ae-00eb2dbb985f" />
+    <projectItemReference itemId="d91c6449-e73e-4f85-bc63-ed893e8c8fb3" />
+    <projectItemReference itemId="2268d969-b148-40f2-be0a-8ff53ea50a8f" />
+    <projectItemReference itemId="4bb0a49a-acfb-4a72-8945-40d8addf8b1f" />
+  </projectItems>
+</feature>
---
+++ CustomDeBusqueda/pkg/Debug/CustomDeBusqueda/CustomDeBusqueda_Feature1/Scripts/App.js
@@ -0,0 +1,136 @@
+﻿//Creacion de variables de acceso público
+var context;
+var web;
+var user;
+var currentItem;
+var hostweburl;
+var appweburl;
+var title;
+
+$(document).ready(function () {
+    appweburl = GetAppSiteUrl();
+    hostweburl = GetHostSiteUrl();
+
+    var scriptbase = hostweburl + "/_layouts/15/";
+    $.getScript(scriptbase + "SP.RequestExecutor.js");
+
+    var clientContext = new SP.ClientContext.get_current();
+    var parentCtx = new SP.AppContextSite(clientContext, hostweburl);
+    var web = parentCtx.get_web();
+    clientContext.load(web);
+    var listId = GetListId();
+    list = web.get_lists().getById(listId);
+    clientContext.load(list);
+    var itemId = GetItemId();
+    currentItem = list.getItemById(itemId);
+    clientContext.load(currentItem);
+    clientContext.executeQueryAsync(onListLoadSucceeded, onRequestFailed);
+});
+
+function onListLoadSucceeded() {
+    title = currentItem.get_fieldValues().Name;
+    getSearchResults(title);
+}
+
+function onRequestFailed(sender,args) {
+    alert('Error:' + args.get_message());
+}
+
+
+//Función para realizar la búsqueda en la lista por el nombre del documento y carga la información en una capa
+function DoSearch() {
+    var query = $('#txtSearch').val();
+    getSearchResults(query);
+}
+//Funcion para obteenr los resultados de la busqueda (la llamada a la API)
+function getSearchResults(queryText) {
+    $('#search-title').text('Resultados para la busqueda[' + queryText + ']');
+
+    var searchUrl = appweburl + "/_api/search/query?querytext='" + queryText + "'&trimduplicates=false";
+    var executor = new SP.RequestExecutor(appweburl);
+    executor.executeAsync({
+        url: searchUrl,
+        method: "GET",
+        headers: { "Accept": "application/json; odata=verbose" },
+        success: onGetSearchResultsSuccess,
+        error: onGetSearchResultsFail
+    });
+}
+//Funcion para cuando la busqueda es satisfactoria
+function onGetSearchResultsSuccess(data) {
+    var jsonObject = JSON.parse(data.body);
+    var results = jsonObject.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results;
+    if (results.length == 0) {
+        $('#search-results').text('No se han encontrado coincidencias');
+    } else {
+        var searchResultsHtml = '';
+        $.each(results, function(index, result) {
+            searchResultsHtml += "<a target='_blank' href='" + result.Cells.results[6].Value + "'>" + result.Cells.results[6].Value + "</a><br/>";
+        });
+        $("#search-results").html(searchResultsHtml);
+    }
+}
+//Funcion para cuando la busqueda de datos es erronea
+function onGetSearchResultsFail(data,errorCode,errorMessage) {
+    $('#search-results').text('Un error ha ocurrido durante la busqueda -' + errorMessage);
+}
+
+function getQueryStringParameter(paramToRetrieve) {
+    var params = document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve)
+            return singleParam[1];
+    }
+}
+
+function GetListId() {
+    return decodeURIComponent(getQueryStringParameter("ListID"));
+}
+
+function GetHostSiteUrl() {
+    return decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+}
+
+function GetAppSiteUrl() {
+    return decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+}
+
+function GetItemId() {
+    return getQueryStringParameter("ItemID");
+}
+
+
+
+//'use strict';
+
+//ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+//function initializePage()
+//{
+//    var context = SP.ClientContext.get_current();
+//    var user = context.get_web().get_currentUser();
+
+//    // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+//    $(document).ready(function () {
+//        getUserName();
+//    });
+
+//    // This function prepares, loads, and then executes a SharePoint query to get the current users information
+//    function getUserName() {
+//        context.load(user);
+//        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+//    }
+
+//    // This function is executed if the above call is successful
+//    // It replaces the contents of the 'message' element with the user name
+//    function onGetUserNameSuccess() {
+//        $('#message').text('Hello ' + user.get_title());
+//    }
+
+//    // This function is executed if the above call fails
+//    function onGetUserNameFail(sender, args) {
+//        alert('Failed to get user name. Error:' + args.get_message());
+//    }
+//}
